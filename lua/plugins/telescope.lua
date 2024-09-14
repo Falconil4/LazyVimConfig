0c42836e765a6e33806a9a925323e08b6e53bebb
falco <>
2024-09-14 20:05:41 +0200

    improve telescope navigation

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,14 @@
 return {
   "nvim-telescope/telescope.nvim",
+  opts = function(_, opts)
+    print(opts.defaults.mappings)
+    opts.defaults.mappings = vim.tbl_deep_extend("force", opts.defaults.mappings, {
+      i = {
+        ["<C-j>"] = require("telescope.actions").move_selection_next,
+        ["<C-k>"] = require("telescope.actions").move_selection_previous,
+      },
+    })
+  end,
   keys = {
     {
       "<leader>fw",
