65bca6bd7857222dd6b90de536c2dff0f5664315
falco <>
2024-09-15 22:34:42 +0200

    improve c# lsp

--- lua/plugins/lsp-csharp.lua
+++ lua/plugins/lsp-csharp.lua
@@ -17,7 +17,28 @@ return {
           },
           RoslynExtensionsOptions = {
             EnableImportCompletion = true,
+            EnableAsyncCompletion = true,
+            EnableAnalyzersSupport = true,
           },
+        },
+        handlers = {
+          ["textDocument/publishDiagnostics"] = function(_, result, ctx, config)
+            if not result or vim.tbl_isempty(result.diagnostics) then
+              return
+            end
+
+            local ignore_list = { "IDE0160" }
+            local filtered_diagnostics = {}
+
+            for _, diagnostic in ipairs(result.diagnostics) do
+              if not vim.tbl_contains(ignore_list, diagnostic.code) then
+                table.insert(filtered_diagnostics, diagnostic)
+              end
+            end
+
+            result.diagnostics = filtered_diagnostics
+            vim.lsp.handlers["textDocument/publishDiagnostics"](_, result, ctx, config)
+          end,
         },
       },
     },
