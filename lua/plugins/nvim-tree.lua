31fce51ae0cc8ba08fc4456d3c78c7157af6d943
falco <>
2024-09-15 14:47:38 +0200

    swap neotree to nvimtree

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    enabled = false,
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    config = function()
+      local function my_on_attach(bufnr)
+        local api = require("nvim-tree.api")
+
+        local function opts(desc)
+          return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+
+        -- default mappings
+        api.config.mappings.default_on_attach(bufnr)
+
+        -- custom mappings
+        vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
+        vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close"))
+      end
+
+      vim.keymap.set("n", "<Leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "File explorer" })
+
+      require("nvim-tree").setup({
+        on_attach = my_on_attach,
+        update_focused_file = {
+          enable = true,
+        },
+      })
+    end,
+  },
+}
